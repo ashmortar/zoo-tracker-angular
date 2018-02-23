d5413632f5d43dd522e0d343c546f0aa9b9aefc8
Aaron Ross <ashmortar@gmail.com>
2018-02-23 09:40:34 -0800

    get project structure up and running

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>zoo</h1>
+  `
+})
+
+export class AppComponent{
+
+}
